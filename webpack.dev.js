06db74b121d45057eb7ffbac30740fb618c3d68b
Jesse Devaney <jessesdevaney@gmail.com>
2021-05-02 21:35:10 -0400

    Minify JS, CSS, and HTML in production
    - Extracted CSS into own file in production
    - Minified CSS in production
    - Added TerserJS back in to minify JS in production
    - Minified HTML in production as well

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const common = require("./webpack.common");
 const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "development",
@@ -8,5 +9,22 @@ module.exports = merge(common, {
     filename: "static/js/[name].bundle.js",
     path: path.resolve(__dirname, "build"),
     assetModuleFilename: "static/imgs/[name][ext]",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", //3. Insert styles into DOM
+          "css-loader", //2. Turns css into commonjs
+          "sass-loader", //1. Turns sass into css
+        ],
+      },
+    ],
   },
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,11 @@
 const path = require("path");
 const common = require("./webpack.common");
 const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -10,5 +14,31 @@ module.exports = merge(common, {
     path: path.resolve(__dirname, "build"),
     assetModuleFilename: "static/imgs/[name].[hash][ext]",
   },
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      minify: {
+        removeAttributeQuotes: true,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: "static/css/[name].[contenthash].css",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, //3. Insert styles into DOM
+          "css-loader", //2. Turns css into commonjs
+          "sass-loader", //1. Turns sass into css
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+  },
 });
